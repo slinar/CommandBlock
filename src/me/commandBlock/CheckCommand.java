aa7a80a4173dacfcb5334bb197852e2ee16e93db
slinar <wenrouleier@gmail.com>
2018-11-23 04:15:35 +0800

    增加匹配子命令，增加玩家执行命令被拒绝时的提示信息

--- src/me/commandBlock/CheckCommand.java
+++ src/me/commandBlock/CheckCommand.java
@@ -51,4 +51,5 @@ public final class CheckCommand {
     private static String tr(String string) {
         return string.trim().replaceAll(" {2,}", " ");
     }
+    
 }
--- src/me/commandBlock/CommandListener.java
+++ src/me/commandBlock/CommandListener.java
@@ -18,4 +18,5 @@ public class CommandListener implements Listener {
             }
         }
     }
+    
 }
